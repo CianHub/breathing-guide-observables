d256ad9d33ca644c0991994325c6b9bffd4ff7df
Cian O'Gralaigh <ihatecian@gmail.com>
2019-01-01 18:15:11 +0000

    Generate app and write breathing guide functionality and styles
    Added app structure, styled circle and animations, added observable functionality.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, Observer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'breathing-guide';
+export class AppComponent implements OnInit {
+  instruction = 'Breathe in';
+
+  constructor() {}
+
+  ngOnInit() {
+    const myObserv = Observable.create((observer: Observer<any>) => {
+      setTimeout(() => {
+        observer.next('Breathe Out');
+      }, 5000);
+      setTimeout(() => {
+        observer.complete();
+      }, 10000);
+    });
+
+    const firstSub = myObserv.subscribe(
+      (data: string) => {
+        this.instruction = data;
+      },
+      (error: string) => {
+        this.instruction = error;
+      },
+      () => {
+        this.instruction = 'Breath In';
+      }
+    );
+
+    setInterval(() => {
+      const sub = myObserv.subscribe(
+        (data: string) => {
+          this.instruction = data;
+        },
+        (error: string) => {
+          this.instruction = error;
+        },
+        () => {
+          this.instruction = 'Breath In';
+        }
+      );
+    }, 10000);
+  }
 }
